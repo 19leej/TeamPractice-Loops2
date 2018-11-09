30218b750ae726e1aa63bd1f3b201d3f302ce2b1
Emily Berger <20bergee@elmbrookstudents.org>
2018-11-09 14:30:23 -0600

    Classes

---
+++ src/FiveNumbers2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class FiveNumbers2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		int count=0;
+		int total=0;
+		for (count=1; count<=5; count++){
+			System.out.println("Enter a number:");
+			int number=input.nextInt();
+			total+=number;
+		}
+		System.out.println(total);
+	}
+
+}
